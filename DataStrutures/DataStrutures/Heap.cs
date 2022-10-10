1930e43367f6a0181c87f1b01e77fb0a6b1ec3da
touya187842 <touya187842@yahoo.co.jp>
2022-10-10 13:15:49 +0800

    Add Heap

---
+++ DataStrutures/DataStrutures/Heap.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStrutures;
+
+public class Heap<T>
+{
+    private T[] Items;
+    private readonly IComparer<T> Comparer;
+    public int Count { get; private set; }
+
+    public Heap(IComparer<T> comparer)
+    {
+        Comparer = comparer;
+        Items = new T[7];
+        Count = 0;
+    }
+
+    public bool TryPeek(out T? item)
+    {
+        if (Count < 1)
+        {
+            item = default;
+            return false;
+        }
+
+        item = Items[0];
+        return true;
+    }
+
+    public bool TryPop(out T? item)
+    {
+        if (Count < 1)
+        {
+            item = default;
+            return false;
+        }
+
+        item = Items[0];
+        ArrangeDown(ref Items[--Count], 0);
+        return true;
+    }
+
+    private void ArrangeDown(ref T item, int index)
+    {
+        int right = (index << 1) + 2;
+        if (right <= Count)
+        {
+            if (Comparer.Compare(Items[right], item) < 0)
+            {
+                var temp = item;
+                Items[index] = Items[right];
+                Items[right] = temp;
+
+                ArrangeDown(ref Items[right], right);
+                
+                item = Items[index];
+            }
+        }
+
+        int left = (index << 1) + 1;
+        if (left <= Count)
+        {
+            if (Comparer.Compare(Items[left], item) < 0)
+            {
+                var temp = item;
+                Items[index] = Items[left];
+                Items[left] = temp;
+
+                ArrangeDown(ref Items[left], left);
+            }
+        }
+    }
+
+    public void Push(T item)
+    {
+        EnsureSpace();
+
+        Items[Count] = item;
+
+        ArrangeUp(ref Items[Count], Count);
+        Count++;
+    }
+
+    private void ArrangeUp(ref T item, int index)
+    {
+        int parent = (index - 1) >> 1;
+        if (parent < 0)
+        {
+            return;
+        }
+
+        if (Comparer.Compare(Items[parent], item) < 0)
+        {
+            return;
+        }
+        var temp = item;
+        item = Items[parent];
+        Items[parent] = temp;
+        ArrangeUp(ref Items[parent], parent);
+    }
+
+    private void EnsureSpace()
+    {
+        if (Count < Items.Length)
+        {
+            return;
+        }
+
+        var newArray = new T[(Items.Length << 1) + 1];
+        Array.Copy(Items, 0, newArray, 0, Count);
+        Items = newArray;
+    }
+}
