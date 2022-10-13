ad6a42559a2c053ddf92e7b8e5fcbf94e60a49f8
touya187842 <touya187842@yahoo.co.jp>
2022-10-13 23:00:15 +0800

    Add HeapTest Arg Source

---
+++ DataStrutures/DataStrutureTests/HeapTestsSource.cs
@@ -0,0 +1,22 @@
+﻿using DataStrutures;
+using System.Collections.Generic;
+using static DataStrutureTests.HeapWithRefObjTests;
+
+namespace DataStrutureTests;
+
+public class HeapTestsSource
+{
+    public static object[] HeapTestsArgs = {
+        new object[] { new Heap<int>(Comparer<int>.Default), 8 },
+        new object[] { new HeapNode<int>(Comparer<int>.Default), 8 },
+        new object[] { new Heap<int>(Comparer<int>.Default), 100 },
+        new object[] { new HeapNode<int>(Comparer<int>.Default), 100 },
+    };
+
+    public static object[] HeapWithRefObjTestsArgs = {
+        new object[] { new Heap<Foo>(new FooComparer()), 8 },
+        new object[] { new HeapNode<Foo>(new FooComparer()), 8 },
+        new object[] { new Heap<Foo>(new FooComparer()), 100 },
+        new object[] { new HeapNode<Foo>(new FooComparer()), 100 },
+    };
+}
