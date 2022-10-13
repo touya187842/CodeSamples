c80dd61100d4f41a4dc28b2e671865c8e23a09cd
touya187842 <touya187842@yahoo.co.jp>
2022-10-13 21:40:32 +0800

    Add IHeap

---
+++ DataStrutures/DataStrutures/IHeap.cs
@@ -0,0 +1,9 @@
+﻿namespace DataStrutures;
+
+public interface IHeap<T>
+{
+    public int Count { get; }
+    public bool TryPeek(out T? item);
+    public bool TryPop(out T? item);
+    public void Push(T item);
+}
