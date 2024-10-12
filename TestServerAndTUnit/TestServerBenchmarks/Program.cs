658486a67beb33d587ed5b07a56d9486da21bee8
touya187842 <touya187842@yahoo.co.jp>
2024-10-12 14:24:52 +0800

    impl demo benchmark

--- TestServerAndTUnit/TestServerBenchmarks/Program.cs
+++ TestServerAndTUnit/TestServerBenchmarks/Program.cs
@@ -1,9 +1,47 @@
-﻿namespace TestServerBenchmarks;
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Microsoft.AspNetCore.TestHost;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TestServerAndTUnit;
+
+namespace TestServerBenchmarks;
 
 public abstract class Program
 {
     public static void Main(string[] args)
     {
+        BenchmarkRunner.Run<Benchmark>();
+    }
+}
 
+public class Benchmark
+{
+    private TestServer? TestServer;
+    private HttpClient? HttpClient;
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        TestServer = TestServerAndTUnits.GetMirrorTestServer();
+    }
+
+    [GlobalCleanup]
+    public void GlobalCleanup()
+    {
+        TestServer?.Dispose();
+    }
+
+    [IterationSetup]
+    public void IterationSetup()
+    {
+        HttpClient = TestServer!.CreateClient();
+    }
+
+    [Benchmark]
+    public async Task Method()
+    {
+        var httpClient = HttpClient!;
+        _ = await httpClient.GetAsync("/api");
     }
 }
